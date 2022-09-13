39b48c5f4bb94356df44b51241a92b78ae2eeb3e
Naisato <Natdadai.t@ku.th>
2022-09-13 09:56:38 +0700

    Install livewire and sass

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <title>Art Miracle</title>
+    @vite('resources/css/app.css','resources/scss/app.scss', 'resources/js/app.js')
+    @livewireStyles
+</head>
+<body>
+<nav>
+
+</nav>
+
+<div class="mx-auto max-w-6xl">
+    @yield('content')
+</div>
+    @livewireScripts
+</body>
+</html>
