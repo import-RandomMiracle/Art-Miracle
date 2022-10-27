b6eb6c4d5ba416d7ffe0c6435e50f2600afd267f
FEELONE <thinnarat.j@ku.th>
2022-10-27 13:06:40 +0700

    layout
    Layout : User Profile

---
+++ resources/views/layouts/info.blade.php
@@ -0,0 +1,36 @@
+<div>
+    <ul class="flex justify-center mt-40 space-x-7">
+        <li>
+            <img class="rounded-full" src="/images/Profile-2.png" alt="UserProfile" height="" width="200" >
+        </li>
+        <li>
+            <div class="space-y-5">
+                <div class="flex space-x-10">
+                    <h1 class="text-white text-xl font-bold">Miracle Artist</h1>
+                    <button class="editProfileButton text-white text-sm">Edit Profile</button>
+                    <a href="wallet">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#FFFFFF" class="bi bi-wallet2" viewBox="0 0 16 16">
+                            <path d="M12.136.326A1.5 1.5 0 0 1 14 1.78V3h.5A1.5 1.5 0 0 1 16 4.5v9a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 13.5v-9a1.5 1.5 0 0 1 1.432-1.499L12.136.326zM5.562 3H13V1.78a.5.5 0 0 0-.621-.484L5.562 3zM1.5 4a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-13z"/>
+                        </svg>
+                    </a>
+                    <a href="reportUser">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#FFCB44" class="bi bi-flag-fill" viewBox="0 0 16 16">
+                            <path d="M14.778.085A.5.5 0 0 1 15 .5V8a.5.5 0 0 1-.314.464L14.5 8l.186.464-.003.001-.006.003-.023.009a12.435 12.435 0 0 1-.397.15c-.264.095-.631.223-1.047.35-.816.252-1.879.523-2.71.523-.847 0-1.548-.28-2.158-.525l-.028-.01C7.68 8.71 7.14 8.5 6.5 8.5c-.7 0-1.638.23-2.437.477A19.626 19.626 0 0 0 3 9.342V15.5a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 1 0v.282c.226-.079.496-.17.79-.26C4.606.272 5.67 0 6.5 0c.84 0 1.524.277 2.121.519l.043.018C9.286.788 9.828 1 10.5 1c.7 0 1.638-.23 2.437-.477a19.587 19.587 0 0 0 1.349-.476l.019-.007.004-.002h.001"/>
+                        </svg>
+                    </a>
+                </div>
+                <ul class="space-y-5">
+                    <li class="flex justify-center space-x-20 text-white">
+                        <p>post</p>
+                        <p>followers</p>
+                        <p>following</p>
+                    </li>
+                    <li>
+                        <p class="text-gray-300">Hello my name is miracle you can see my artwork on profile now</p>
+                    </li>
+                </ul>
+            </div>
+        </li>
+
+    </ul>
+</div>
---
+++ resources/views/layouts/user.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <title>Art Miracle</title>
+    @vite('resources/css/app.css','resources/scss/app.scss', 'resources/js/app.js')
+</head>
+<body class="min-h-screen backgroundHome">
+    @include('layouts.navbar')
+    @include('layouts.info')
+
+
+
+    <div class="mx-auto max-w-6xl">
+        @yield('content')
+    </div>
+</body>
+</html>
