4ecc256475e4ae4ed61b55c2890cedd8ac97f057
FEELONE <thinnarat.j@ku.th>
2022-10-29 22:02:17 +0700

    artist

--- resources/views/layouts/postartist.blade.php
+++ resources/views/layouts/postartist.blade.php
@@ -1,9 +1,7 @@
 @foreach($users['data'] as $user)
     <div class="postimg">
         <img class="postImage" src="/images/Artist-1.png" alt="Miracle Work" height="" width="300" >
-        <h1 class="ml-2 textArtistPost font-bold">
-            Artist Name : {{$user['display_name']}}
-        </h1>
-        <p class="ml-2 textArtistPost text-sm">Follower : 15000 people</p>
+        <h1 class="truncate ml-2 textArtistPost font-bold">{{$user['display_name']}}</h1>
+        <p class="ml-2 textArtistPost text-sm">Follower : {{$user['follower_count']}} people</p>
     </div>
 @endforeach
