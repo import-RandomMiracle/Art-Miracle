79c57a31995e58bb05be9ba2e0c296f6698f43f1
Mekpearaphat <78583538+Mekpearaphat@users.noreply.github.com>
2022-10-30 20:14:07 +0700

    commit view popup confirm transfer

---
+++ resources/views/layouts/confirmtransfer.blade.php
@@ -0,0 +1,27 @@
+<div class="flex justify-center">
+    <div class="popupBanRectangle mt-48">
+        <div class="mt-16">
+            <div class="text-4xl text-center fontBanColor font-bold mt-2">
+                You Confirm To Transfer
+            </div>
+            <div class="justify-center">
+                <div class="mt-4 ml-10 mr-10 text-center">
+                    <h1 class="fontBanColor mr-2">If you're confirmed the transfer,it can't be edited.</h1>
+                </div>
+            </div>
+
+            <div class="grid grid-cols-2 place-items-stretch ">
+                <div class="mt-14 m-14">
+                    <button class="yesButton font-bold text-2xl">
+                        YES
+                    </button>
+                </div>
+                <div class="mt-14 m-14">
+                    <button class="noButton font-bold text-2xl">
+                        NO
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div>
-        @include('layouts.reportproduct')
+        @include('layouts.confirmtransfer')
     </div>
 
 @endsection
