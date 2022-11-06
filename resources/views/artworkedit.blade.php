1f429bb33c429f397e35e211d0f097e75c742a6b
Théo Teenster <thewro11@gmail.com>
2022-11-07 00:10:43 +0700

    add httpRequest in Artwork

--- resources/views/artworkedit.blade.php
+++ resources/views/artworkedit.blade.php
@@ -9,23 +9,23 @@
                     <h1 class="text-red-500">Deleting is a permanent cannot be recovered.</h1>
                 </div>
                 <div class="flex flex-row">
-                    <div class="flex justify-center space-x-5 m-5 hiddenButton">
-                        <button class="deleteButton text-xl font-bold">
+                    <div class="flex justify-center m-5 space-x-5 hiddenButton">
+                        <button class="text-xl font-bold deleteButton">
                             DELETE
                         </button>
                     </div>
-                    <div class="flex justify-center space-x-5 m-5 hiddenButton">
-                        <button class="buyNowButton text-xl font-bold">
+                    <div class="flex justify-center m-5 space-x-5 hiddenButton">
+                        <button class="text-xl font-bold buyNowButton">
                             SAVE
                         </button>
                     </div>
                 </div>
 
             </div>
-            <form method="POST" action="">
+            <form method="POST" action="{{ route('edit.artwork', [  'price' => $artwork->price, 'description' => $artwork->description, 'category' => $artwork->categories  ]) }}">
                 @csrf
 
-                <div class="flex flex-col text-white font-bold text-md space-y-5 mr-6 ml-6">
+                <div class="flex flex-col ml-6 mr-6 space-y-5 font-bold text-white text-md">
                     <div>
                         <h1>Artwork Name</h1>
                         <textarea type="text" name="artwork_name" id="artwork_name" cols="30" rows="1" class="w-full " required autofocus ></textarea>
@@ -39,16 +39,16 @@
                         <textarea name="artwork_name" id="artwork_name" cols="30" rows="5" class="w-full"></textarea>
                     </div>
                 </div>
-                <div class="hidden ml-28 marginCommentLeft text-center showButton">
+                <div class="hidden text-center ml-28 marginCommentLeft showButton">
                     <h1 class="text-red-500">Deleting is a permanent cannot be recovered.</h1>
                 </div>
-                <div class="hidden flex justify-center space-x-5 m-5 mt-1 showButton">
-                    <button class="deleteButton text-xl font-bold">
+                <div class="flex justify-center hidden m-5 mt-1 space-x-5 showButton">
+                    <button name="delete" type="submit" class="text-xl font-bold deleteButton">
                         DELETE
                     </button>
                 </div>
-                <div class="hidden flex justify-center space-x-5 m-5 mt-1 showButton">
-                    <button class="buyNowButton text-xl font-bold">
+                <div class="flex justify-center hidden m-5 mt-1 space-x-5 showButton">
+                    <button name="save" type="submit" class="text-xl font-bold buyNowButton">
                         SAVE
                     </button>
                 </div>
