e5cb1d701fa612eac3d676dbdacef4c71f93278b
FEELONE <thinnarat.j@ku.th>
2022-11-06 21:53:07 +0700

    fix config

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,18 +19,18 @@
     </ul>
 
     <div class="flex justify-center m-20">
-        <div class="rectangle">
-            <ul class="flex justify-center mt-7">
-                <li>
-                    <button class="recButton pause mr-10 text-xl align-center text-white" onclick="document.location='/artwork'">
+        <div >
+            <ul class="flex justify-center mt-7 rectangle">
+                <li class="mt-2.5 m-2.5">
+                    <button class="recButton pause text-xl align-center text-white" onclick="document.location='/artwork'">
                         <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="#FFFFFF" class="bi bi-palette-fill svgButton" viewBox="0 0 16 16">
                             <path d="M12.433 10.07C14.133 10.585 16 11.15 16 8a8 8 0 1 0-8 8c1.996 0 1.826-1.504 1.649-3.08-.124-1.101-.252-2.237.351-2.92.465-.527 1.42-.237 2.433.07zM8 5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm4.5 3a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zM5 6.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm.5 6.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
                         </svg>
                         <p>Artwork</p>
                     </button>
                 </li>
-                <li>
-                    <button class="recButton mr-10 text-xl text-white" onclick="document.location='/artist'">
+                <li class="mt-2.5 m-2.5">
+                    <button class="recButton text-xl text-white" onclick="document.location='/artist'">
                         <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="#FFFFFF" class="bi bi-person-square svgButton" viewBox="0 0 16 16">
                             <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
                             <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1v-1c0-1-1-4-6-4s-6 3-6 4v1a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12z"/>
@@ -38,8 +38,8 @@
                         Artists
                     </button>
                 </li>
-                <li>
-                    <button class="recButton mr-10 text-xl text-white" onclick="document.location='/category'">
+                <li class="mt-2.5 m-2.5">
+                    <button class="recButton text-xl text-white" onclick="document.location='/category'">
                         <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="#FFFFFF" class="bi bi-postcard-fill svgButton" viewBox="0 0 16 16">
                             <path d="M11 8h2V6h-2v2Z"/>
                             <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm8.5.5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7ZM2 5.5a.5.5 0 0 0 .5.5H6a.5.5 0 0 0 0-1H2.5a.5.5 0 0 0-.5.5ZM2.5 7a.5.5 0 0 0 0 1H6a.5.5 0 0 0 0-1H2.5ZM2 9.5a.5.5 0 0 0 .5.5H6a.5.5 0 0 0 0-1H2.5a.5.5 0 0 0-.5.5Zm8-4v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5Z"/>
@@ -47,8 +47,8 @@
                         Category
                     </button>
                 </li>
-                <li>
-                    <button class="recButton mr-10 text-xl text-white" onclick="document.location='/tag'">
+                <li class="mt-2.5 m-2.5">
+                    <button class="recButton text-xl text-white" onclick="document.location='/tag'">
                         <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="#FFFFFF" class="bi bi-tags-fill svgButton" viewBox="0 0 16 16">
                             <path d="M2 2a1 1 0 0 1 1-1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 2 6.586V2zm3.5 4a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/>
                             <path d="M1.293 7.793A1 1 0 0 1 1 7.086V2a1 1 0 0 0-1 1v4.586a1 1 0 0 0 .293.707l7 7a1 1 0 0 0 1.414 0l.043-.043-7.457-7.457z"/>
@@ -56,8 +56,8 @@
                         Tag
                     </button>
                 </li>
-                <li>
-                    <button class="recButton mr-10 text-xl text-white" onclick="document.location='/account'">
+                <li class="mt-2.5 m-2.5">
+                    <button class="recButton text-xl text-white" onclick="document.location='/account'">
                         <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="#FFFFFF" class="bi bi-archive-fill svgButton" viewBox="0 0 16 16">
                             <path d="M12.643 15C13.979 15 15 13.845 15 12.5V5H1v7.5C1 13.845 2.021 15 3.357 15h9.286zM5.5 7h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1zM.8 1a.8.8 0 0 0-.8.8V3a.8.8 0 0 0 .8.8h14.4A.8.8 0 0 0 16 3V1.8a.8.8 0 0 0-.8-.8H.8z"/>
                         </svg>
@@ -75,10 +75,7 @@
             <p class="text-sm text-amber-400 text-center">View All Artists</p>
         </a>
     </div>
-    <div class="flex justify-center" class="relative w-full flex gap-6 snap-x snap-mandatory overflow-x-auto pb-5">
-        <div class="snap-start scroll-mx-6 shrink-0">
-            <div class="shrink-0 w-0"></div>
-        </div>
+    <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4" >
         @include('layouts.posttop4artist')
         {{-- <ul class="flex justify-center">
             <li>
@@ -117,10 +114,7 @@
             <p class="text-sm text-amber-400 text-center">View All Artwork</p>
         </a>
     </div>
-    <div class="flex justify-center" class="relative w-full flex gap-6 snap-x snap-mandatory overflow-x-auto pb-5">
-        <div class="snap-start scroll-mx-6 shrink-0">
-            <div class="shrink-0 w-0"></div>
-        </div>
+    <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         @include('layouts.posttop4')
         {{-- <ul class="flex justify-center">
             <li>
