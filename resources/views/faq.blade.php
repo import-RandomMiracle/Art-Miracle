8d28f7c75dc548dd3fcdde887705c8609d24400b
chatchawan <chatchawan.mu@ku.th>
2022-11-06 22:58:16 +0700

    commit answer

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -4,12 +4,20 @@
     <div>
         <h1>FAQs</h1>
 
-        <div>
+        <>
             <p>What is this website used for?</p>
+            <div>Use to sell your ฟพะ work online.</div>
             <p>How to sell Art work?</p>
+            <div>1. Register as a Artist.
+                2. Upload your work.
+                3. Wait for the approval of the work.
+                4. You can sell your work.</div>
             <p>What kind of Art work can I sell?</p>
+            <div>Any kind of Art work.</div>
             <p>How can I pay?</p>
+            <div>You can purchase with AC currency only, such currency can be redeemed within Art Miracle.</div>
             <p>When will I receive my money?</p>
+            <div>After the buyer has paid, the money AC will be transferred to your Wallet account. You can chang AC to THB within Art Miracle </div>
         </div>
     </div>
 
