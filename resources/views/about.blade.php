3ae5fb180dfd7636824e89cef94416d4c836c153
chatchawan <chatchawan.mu@ku.th>
2022-11-06 18:45:23 +0700

    commit benefit about and contact

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,6 +2,40 @@
 
 @section('content')
     <div>
+
+    <h1>Art is a magic</h1>
+    <p>
+    Art Miracle is a platform collection of attractive works of art in one place.
+    </p>
+
+    <div>
+        <h2>Our Mission</h2>
+        <p>
+        We are here to help you find the best art pieces for your home or office.
+        </p>
+    </div>
+
+    <div>
+        <h2>Our Vision</h2>
+        <p>
+        We want to make art accessible to everyone.
+        </p>
+    </div>
+
+    <div>
+        <h2>Our Values</h2>
+        <p>
+        We value our customers and we want to make them happy.
+        </p>
+    </div>
+
+    <div>
+        <h2>Our Team</h2>
+        <p>
+        We are a team of art lovers.
+        </p>
+    </div>
+
         
     </div>
 @endsection
